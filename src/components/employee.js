083fab881a2d35bb03c384db52b798eb2897dde2
yotamba12 <yotambaram@gmail.com>
2020-05-09 13:29:12 -0700

    updade

--- src/components/employee.js
+++ src/components/employee.js
@@ -2,22 +2,6 @@
     import Card from "./card";
     
 
-
-function ResultList(props) {
-  return (
-    <ul className="list-group">
-      {props.results.map(result => (
-        <li className="list-group-item" key={result.id}>
-          <img alt={result.title} className="img-fluid" src={result.images.original.url} />
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-
-
-
     
     function Employee(props) {
       console.log("EMPLOEE COMPONENT", props.employeeState)
