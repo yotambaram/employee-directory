ddf300216c0e7efe4bbfa0f59c2c0c3026f62613
yotamba12 <yotambaram@gmail.com>
2020-05-10 10:56:00 -0700

    sort by first name is working

--- src/components/container.js
+++ src/components/container.js
@@ -8,17 +8,25 @@ import API from "../utils/api"
 
 
 function Container() {
+  // permanent / defulte state
+  const [permEmployeeState, setPermEmployeeState] = useState([])
+  // state to present
   const [employeeState, setEmployeeState] = useState([])
-  const [workEmployeeState, setWorkEmployeeState] = useState([])
+  //search/filter state
   const [searchState, setSearchState] = useState("");
+  //sort state
   const [sortedState, setSortedState] = useState();
+
+
 
   //GET THE LIST FROM API
   useEffect(() => {
     API.getEmployees().then(res => {
       //console.log("FROM API" ,res.data.results)
+      // state for working on
       setEmployeeState(res.data.results)
-      setWorkEmployeeState(res.data.results)
+      // permanent state
+      setPermEmployeeState(res.data.results)
 
     }).catch(err => {
       console.log(err);
@@ -27,7 +35,7 @@ function Container() {
 
   //FILER LIST BY SEARCH
   useEffect(() => {
-    const NewEmployeeState = [...workEmployeeState];
+    const NewEmployeeState = [...permEmployeeState];
     const filteredEmployee = NewEmployeeState.filter(employee => {
       if (employee.name.first.includes(searchState)) {
         return true
@@ -41,11 +49,16 @@ function Container() {
 
   //UPDATE SORTED LIST
   useEffect(() => {
-    //sort condition (if  < || >)
+    //create new list to work on
+    const NewEmployeeState = [...permEmployeeState];
+    //sort  (if  < || >)
     //change the arry 
+    NewEmployeeState.sort((a, b) => a.name.first > b.name.first ? 1 : -1)
+    setEmployeeState(NewEmployeeState);
+  }, [sortedState])
 
-    //setEmployeeState(sortedEmployee);
-  }, [/*sortedState*/])
+
+
 
   //INPUT HUNDEL
   const hundleInput = event => {
@@ -66,7 +79,7 @@ function Container() {
           </form>
         </div>
 
-        <div><button onClick={hundleSort}>Name</button></div>
+        <div><button onClick={hundleSort}>sort</button></div>
         <a>content</a>
         <Employee employeeState={employeeState} />
 
